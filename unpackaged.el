746801a677ada6cd6fa076e423aa0953779f3fad
Adam Porter <adam@alphapapa.net>
2019-10-03 21:13:36 -0500

    Add: (flex-fill-paragraph) With prefix, fill to reduce line count

--- unpackaged.el
+++ unpackaged.el
@@ -927,26 +927,37 @@ Deletes the package first to remove obsolete versions."
   "Last fill column used in command `unpackaged/flex-fill-paragraph'.")
 
 ;;;###autoload
-(defun unpackaged/flex-fill-paragraph (&optional unfill)
+(defun unpackaged/flex-fill-paragraph (&optional fewer-lines unfill)
   "Fill paragraph, incrementing fill column to cause a change when repeated.
+The global value of `fill-column' is not modified; it is only
+bound around calls to `fill-paragraph'.
+
 When called for the first time in a sequence, unfill to the
-default `fill-column'.  When called repeatedly, increase
-`fill-column' until filling changes.  With prefix, unfill
-completely.  The global value of `fill-column' is not modified;
-it is only bound around calls to `fill-paragraph'."
+default `fill-column'.
+
+When called repeatedly, increase `fill-column' until filling
+changes.
+
+With one universal prefix, increase `fill-column' until the
+number of lines is reduced.  With two, unfill completely."
   (interactive "P")
-  (let ((fill-column
-         (cond (unfill (setf unpackaged/flex-fill-paragraph-column nil)
-                       most-positive-fixnum)
-               (t (setf unpackaged/flex-fill-paragraph-column
-                        (if (equal last-command this-command)
-                            (or (unpackaged/flex-fill-paragraph--next-fill-column) fill-column)
-                          fill-column))))))
+  (let* ((fewer-lines (or fewer-lines (equal current-prefix-arg '(4))))
+         (unfill (or unfill (equal current-prefix-arg '(16))))
+         (fill-column
+          (cond (unfill (setf unpackaged/flex-fill-paragraph-column nil)
+                        most-positive-fixnum)
+                (t (setf unpackaged/flex-fill-paragraph-column
+                         (if (equal last-command this-command)
+                             (or (unpackaged/flex-fill-paragraph--next-fill-column fewer-lines)
+                                 fill-column)
+                           fill-column))))))
     (fill-paragraph)
     (message "Fill column: %s" fill-column)))
 
-(defun unpackaged/flex-fill-paragraph--next-fill-column ()
-  "Return next `fill-column' value that causes a change in current paragraph."
+(defun unpackaged/flex-fill-paragraph--next-fill-column (&optional fewer-lines)
+  "Return next `fill-column' value.
+If FEWER-LINES is non-nil, reduce the number of lines in the
+buffer, otherwise just change the current paragraph."
   ;; This works well, but because of all the temp buffers, sometimes when called
   ;; in rapid succession, it can cause GC, which can be noticeable.  It would be
   ;; nice to avoid that.  Note that this has primarily been tested on
@@ -954,16 +965,21 @@ it is only bound around calls to `fill-paragraph'."
   (let* ((point (point))
          (source-buffer (current-buffer))
          (mode major-mode)
-         (fill-column (or unpackaged/flex-fill-paragraph-column fill-column)))
+         (fill-column (or unpackaged/flex-fill-paragraph-column fill-column))
+         (old-fill-column fill-column)
+         (hash (unless fewer-lines
+                 (buffer-hash)))
+         (original-num-lines (when fewer-lines
+                               (line-number-at-pos (point-max)))))
     (with-temp-buffer
       (delay-mode-hooks
         (funcall mode))
       (insert-buffer-substring source-buffer)
       (goto-char point)
-      (cl-loop with old-fill-column = fill-column
-               with hash = (buffer-hash)
-               while (and (fill-paragraph)
-                          (string= hash (buffer-hash)))
+      (cl-loop while (and (fill-paragraph)
+                          (if fewer-lines
+                              (= original-num-lines (line-number-at-pos (point-max)))
+                            (string= hash (buffer-hash))))
                ;; If filling doesn't change after 100 iterations, abort by returning nil.
                if (> (- fill-column old-fill-column) 100)
                return nil
